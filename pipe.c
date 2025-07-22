bb45d3c28a564c5a49dca5b7f1872e46d27b439a
aditya-am-murthy <aditya.am.murthy@gmail.com>
2025-07-22 11:09:47 -0700

    coded pipe for 1 arg

--- pipe.c
+++ pipe.c
@@ -5,12 +5,17 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+// takes the output of the first process and makes it the input of the second process
 int main(int argc, char *argv[])
 {
-	// takes the output of the first process and makes it the input of the second process
 	if (argc == 1) { // for zero arguments, exit with einval
 		errno = EINVAL;
-
-
+		exit(errno);
+	} else if (argc == 2) { // for one argument, just execute the first command
+		errno = execlp(argv[1], argv[1], NULL);
+		if (errno == -1) {
+			exit(errno);
+		}
+		return 0;
 	}
 }
